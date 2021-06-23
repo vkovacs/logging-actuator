cccb6d5a8b0741a985ae30095446b0b06b378d0e
Viktor Kovacs <viktor.kovacs@siemens.com>
2021-06-23 08:38:39 +0200

    use config server

--- src/main/java/com/siemens/logging/rest/LoggingController.java
+++ src/main/java/com/siemens/logging/rest/LoggingController.java
@@ -1,6 +1,7 @@
 package com.siemens.logging.rest;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoggingController {
 
+    @Value("${message: World}")
+    public String message;
+
     @GetMapping("/log")
     public void log() {
-        log.info("INFO Log");
-        log.debug("DEBUG Log");
+        log.info("INFO Hello " + message);
+        log.debug("DEBUG Hello " + message);
     }
 }
